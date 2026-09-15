f4e37bda0717e030a2d0879b8549d198b840483e
Parryword <101982505+Parryword@users.noreply.github.com>
2026-09-15 10:00:18 +0300

    update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -88,7 +88,7 @@ export default function Home() {
                   "& .MuiOutlinedInput-root": {
                     height: "100%",
                     borderRadius: { xs: 2, sm: "8px 0 0 8px" },
-                    "& fieldset": { borderColor: "#dfe3e8", borderRight: { sm: 0 } },
+                    "& fieldset": { borderColor: "#dfe3e8" },
                   },
                 }}
               >
